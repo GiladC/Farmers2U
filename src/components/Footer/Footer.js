22674dc2b05eaf3e4bc67620bafbd54dfbd50ac5
raipheterson <96655166+raipheterson@users.noreply.github.com>
2023-04-27 01:30:46 +0300

    Add files via upload

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,105 +1,85 @@
 import React from 'react';
-import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import Toolbar from '@mui/material/Toolbar';
+//import styled from 'styled-components';
 //import { Link } from 'react-router-dom';
-import './App.css';
-
-const FooterLine = styled.hr`
-background: #ffcf66;
-height: 220px;
-position: fixed;
-bottom: 0;
-width: 100%;
-`;
-
-const MenuItemHeadline = styled.div`
-  padding: 0 1rem;
-  cursor: pointer;
-  font-weight: bold;
-  font-size: 20px;
-  text-decoration: underline;
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const RightMenu = styled.div`
-  position: absolute;
-  top: 550px;
-  right: 100px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const MidMenu = styled.div`
-  position: absolute;
-  top: 550px;
-  right: 500px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const LeftMenu = styled.div`
-  position: absolute;
-  top: 550px;
-  right: 900px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const MenuItem = styled.div`
-  padding: 0 1rem;
-  cursor: pointer;
-  margin-bottom: 1rem;
-`;
-
-const AllRightsReserved = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: black;
-`;
+import '../../App.css';
+//import { styled } from '@mui/system';
+import Logo from '../../logos/farmers2u_logo.png'
+//import { useTheme } from '@mui/material/styles';
+import { Typography } from '@mui/material'
 
 
 
-function Footer() {
+const Footer = () => {
   return (
-    <>
-      <FooterLine />
-      <RightMenu>
-        <MenuItem>Fruits</MenuItem>
-        <MenuItem>Vegtables</MenuItem>
-        <MenuItem>Whatever we support</MenuItem>
-      </RightMenu>
-      <MidMenu>
-        <MenuItem>Homepage</MenuItem>
-        <MenuItem>Our Farmers</MenuItem>
-        <MenuItem>Luah Moad'ot</MenuItem>
-      </MidMenu>
-      <LeftMenu>
-        <MenuItem>FAQ</MenuItem>
-        <MenuItem>About</MenuItem>
-        <MenuItem>Contact</MenuItem>
-      </LeftMenu>
-      <MenuItemHeadline style={{top: '510px', right: '90px', marginBottom: '20px' }}>What can I buy here?</MenuItemHeadline>
-      <MenuItemHeadline style={{top: '510px', right: '470px', marginBottom: '20px' }}>Website navigation</MenuItemHeadline>
-      <MenuItemHeadline style={{top: '510px', right: '880px', marginBottom: '20px' }}>Something</MenuItemHeadline>
-      <AllRightsReserved>
-        <p>All Rights Reserved to Farmers2U &copy; 2023</p>
-      </AllRightsReserved>
-      <div>
-        <h1>Farmers2U</h1>
-      </div>
-    </>
-  );
+    <div>
+      <Toolbar style={{ position: "fixed", bottom: 0, width: "100%", backgroundColor: "#eeeeee", height: "20vh", color: '#eeeeee'}}>
+      <a href="./pages/MainPage">
+        <img src={Logo} alt='Logo' style={{position: 'absolute', top:"17%", left:"10%", width: "100px"}}></img>
+      </a>  
+      <ul style={{position: 'absolute', top:"5%", left:"90%"}}>
+        <li style={{ paddingBlock: '15px'}}>
+          <Link to = "./pages/MainPage" style={{fontFamily: 'Alef', variant: 'body2', textDecoration: "none", color: "#616161"}}>
+            דף הבית
+          </Link>
+        </li>
+        <li>
+          <Link to = "./pages/BullBoard" style={{fontFamily: 'Alef', variant: 'body2', textDecoration: "none", color: "#616161"}}>
+            לוח מודעות
+          </Link>
+        </li>
+      </ul>
+      <ul style={{position: 'absolute', top:"5%", left:"72%"}}>
+        <li style={{ paddingBlock: '15px'}}>
+          <Link to = "./pages/About" style={{fontFamily: 'Alef', variant: 'body2', textDecoration: "none", color: "#616161"}}>
+            על האתר
+          </Link>
+        </li>
+        <li>
+          <Link to = "./pages/FAQ" style={{fontFamily: 'Alef', variant: 'body2', textDecoration: "none", color: "#616161"}}>
+            שאלות נפוצות
+          </Link>
+        </li>
+      </ul>
+      <ul style={{position: 'absolute', top:"5%", left:"53%"}}>
+      <li style={{ paddingBlock: '15px'}}>
+          <Link to = "./pages/BrowseProducts" style={{fontFamily: 'Alef', variant: 'body2', textDecoration: "none", color: "#616161"}}>
+            חיפוש מוצרים
+          </Link>
+        </li>
+        <li>
+          <Link to = "./pages/OurFarmers" style={{fontFamily: 'Alef', variant: 'body2', textDecoration: "none", color: "#616161"}}>
+            החקלאים שלנו
+          </Link>
+        </li>
+      </ul>
+      <ul style={{position: 'absolute', top:"5%", left:"30%"}}>
+        <li style={{ paddingBlock: '15px'}}>
+          <Link to = "./pages/FarmerLogin" style={{fontFamily: 'Alef', variant: 'body2', textDecoration: "none", color: "#616161"}}>
+            התחברות
+          </Link>
+        </li>
+        <li>
+          <Link to = "./pages/FarmerCreateUser" style={{fontFamily: 'Alef', variant: 'body2', textDecoration: "none", color: "#616161"}}>
+            פתיחת פרופיל חקלאי
+          </Link>
+        </li>
+      </ul>         
+      <Typography fontFamily='Alef' variant='body2' style={{fontFamily: 'Alef', variant: 'body2', position: 'absolute', bottom: 0, left: "50%", transform: 'translateX(-50%)'}}fontSize={15}  margin={"auto"} marginBottom={1} padding={0}  color="text.secondary" align="center">
+      © כל הזכויות שמורות
+      {" | "}
+      Farmers2U
+      {" | "} 
+      {new Date().getFullYear()} 
+      {" | "}
+      <Link to ="./pages/Terms" color="inherit" style={{textDecoration: "none", color: "#616161"}}>
+        תנאי השימוש
+      </Link>
+    </Typography>
+      </Toolbar>
+    </div>
+  )
 }
 
 export default Footer;
