bc9da8179a2e0c3cf8709016d764400c6d523243
GiladC <giladcochavi@gmail.com>
2023-05-27 20:19:08 +0300

    App.js updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,71 +1,90 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBarElements from "./components/Navbar/NavbarElements";
-import Footer from'./components/Footer/Footer';
+import Footer from './components/Footer/Footer';
 import React from 'react';
 import LandPage from './Pages/MainPage/LandPage';
 import FormLogin from './Pages/Forms/FormLogin';
-import faq from './Pages/FAQ';
-import ourfarmers from './Pages/OurFarmers';
+import Faq from './Pages/FAQ';
+import OurFarmers from './Pages/ourfarmers/OurFarmers';
 import AdsPage from './Pages/Bullboard/adsPage';
 import Form from './Pages/Forms/Form';
 import about from './Pages/About';
 import FilterPanel from './components/FilterPanel/FilterPanel';
 import Intro from './Pages/ShowFarmerProfile/intro';
+import ProfileSettings from './Pages/Settings/profileSettings';
 import './App.css'
 
 function App() {
+  const appContainerStyles = {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  };
+
+  const contentContainerStyles = {
+    flexGrow: 1,
+    paddingTop: '0px', // How much padding we want to have for the navbar
+  };
+
   return (
     <Router>
-      <NavBarElements/>
-      <div>
-        <Routes>
-          <Route path='/home' element={
-            <>
-              <LandPage/>
-            </>
-          } />
-          <Route exact path='/signup' element={
-            <>
-              <Form/>
-            </>
-          } />
-          <Route path='/login' element={
-            <>
-              <FormLogin/>
-            </>
-          } />
-          <Route path='/bullboard' element={
-            <>
-              <AdsPage/>
-              <FilterPanel/>
-            </>
-          } />
-          <Route path='/ourfarmers' element={
-            <>
-              <ourfarmers/>
-            </>
-          } />
-          <Route path='/faq' element={
-            <>
-              <faq/>
-            </>
-          } />
-          <Route path='/about' element={
-            <>
-              <about/>
-            </>
-          } />
-          <Route path="/farmerProfile" element={
-            <>
-              <Intro/>
-            </>
-          } />
-          <Route path="/" element={
-            <>
-              <LandPage/>
-            </>
-          } />
-        </Routes>
+      <div style={appContainerStyles}>
+        <NavBarElements />
+        <div style={contentContainerStyles}>
+          <Routes>
+            <Route path='/home' element={
+             <>
+                <LandPage/>
+              </>
+            } />
+            <Route exact path='/signup' element={
+              <>
+                <Form/>
+              </>
+            } />
+            <Route path='/login' element={
+              <>
+                <FormLogin/>
+              </>
+            } />
+            <Route path='/settings' element={
+              <>
+                <ProfileSettings/>
+              </>
+            } />
+            <Route path='/bullboard' element={
+              <>
+                <AdsPage/>
+                <FilterPanel/>
+              </>
+            } />
+            <Route path='/ourfarmers' element={
+              <>
+                <OurFarmers/>
+              </>
+            } />
+            <Route path='/faq' element={
+              <>
+                <faq/>
+              </>
+            } />
+            <Route path='/about' element={
+              <>
+                <about/>
+              </>
+            } />
+            <Route path="/farmerProfile" element={
+              <>
+                <Intro/>
+              </>
+            } />
+            <Route path="/" element={
+              <>
+                <LandPage/>
+              </>
+            } />
+          </Routes>
+        </div>
         <Footer />
       </div>
     </Router>
