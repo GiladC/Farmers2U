d99f7c26f28d9c817809a05e50390aca909ed663
TamirSadovsky <61319079+TamirSadovsky@users.noreply.github.com>
2023-06-26 21:42:09 +0300

    updated openingHours + backend data - still need improve

--- frontend/src/Pages/Forms/FormOpeningHours.js
+++ frontend/src/Pages/Forms/FormOpeningHours.js
@@ -1,23 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Box, Typography, Grid, Paper } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import { useState } from 'react';
-import WorkingHours from '../../components/Settings/workingHours'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { TimeField } from '@mui/x-date-pickers/TimeField';
-
-// export default function WorkingHours({day}) {
-//   const [value, setValue] = React.useState(null);
-//   // const [value2, setValue2] = React.useState(dayjs('2022-04-17T17:300'));
-//   const [value2, setValue2] = React.useState(null);
-
-
-{/*const useStyles = makeStyles({
-  transparentBackground: {
-    background: '#f7f1e5',
-  },
-});*/}
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -25,60 +11,82 @@ const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(1),
   textAlign: 'center',
   color: theme.palette.text.secondary,
-  margin: theme.spacing(1), // Add margin to create space around the text
+  margin: theme.spacing(1),
 }));
 
-function FormOpeningHours(values, handleChange) {
-  {/*const classes = useStyles();*/}
-  console.log(values, handleChange);
+function FormOpeningHours({values, setFormValue}) {
+  const {farm_name, email, google_profile_picture, google_name, google_family_name, 
+  shipping_distance, is_shipping, opening_hours, closing_hours, logo_picture, products_pictures, types_of_products, 
+  farm_pictures, phone_number_official, phone_number_whatsapp, phone_number_telegram, about, address,
+  farmer_name, delivery_details, products, farm_site, facebook, instagram
+  } = values
+  const [openingTimes, setOpeningTimes] = useState(Array(7).fill(null));
+  const [closingTimes, setClosingTimes] = useState(Array(7).fill(null));
 
-  const [openingTimes, setOpeningTimes] = useState(['', '', '', '', '', '', '']); // Opening times for each day
-  const [closingTimes, setClosingTimes] = useState(['', '', '', '', '', '', '']); // Closing times for each day
   const days = ['ראשון', 'שני', 'שלישי', 'רביעי', 'חמישי', 'שישי', 'שבת'];
-  const handleChangeOpeningTime = (dayIndex, inputTime) => {
-    //const inputTime = event.target.value;
-    console.log(inputTime);
 
-    // Check if the entered time matches the format "HH:MM"
-    const isValidTime = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/.test(inputTime);
-
-    if (isValidTime || inputTime === '') {
-      const newOpeningTimes = [...openingTimes];
-      newOpeningTimes[dayIndex] = inputTime;
-      setOpeningTimes(newOpeningTimes);
-      alert(newOpeningTimes)
-
-    }
-    console.log(openingTimes);
-
-  };
-
-  const handleChangeClosingTime = (dayIndex, inputTime) => {
-    //const inputTime = event.target.value;
-
-    // Check if the entered time matches the format "HH:MM"
-    const isValidTime = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/.test(inputTime);
-
-    if (isValidTime || inputTime === '') {
-      const newClosingTimes = [...closingTimes];
-      newClosingTimes[dayIndex] = inputTime;
-      setClosingTimes(newClosingTimes);
-    }
-    console.log(closingTimes);
-
-  };
-  const resetOpeningTime = (dayIndex) => {
-    const newOpeningTimes = [...openingTimes];
-    newOpeningTimes[dayIndex] = '';
-    setOpeningTimes(newOpeningTimes);
+  const handleChangeOpeningTime = (index, newValue) => {
+    const timeStr = formatTimeValue(newValue);
+  
+    setOpeningTimes(prevTimes => {
+      const newTimes = [...prevTimes];
+      newTimes[index] = timeStr;
+      return newTimes;
+    });
   };
   
-  const resetClosingTime = (dayIndex) => {
-    const newClosingTimes = [...closingTimes];
-    newClosingTimes[dayIndex] = '';
-    setClosingTimes(newClosingTimes);
+  const handleChangeClosingTime = (index, newValue) => {
+    const timeStr = formatTimeValue(newValue);
+  
+    setClosingTimes(prevTimes => {
+      const newTimes = [...prevTimes];
+      newTimes[index] = timeStr;
+      return newTimes;
+    });
   };
   
+  const formatTimeValue = value => {
+    if (value && value.length >= 2) {
+      const hours = value.slice(0, 2);
+      const minutes = value.slice(3, 5);
+      return `${hours}:${minutes}`;
+    }
+    return value;
+  };
+
+useEffect(() => {
+  if (openingTimes.every(time => time)) { // Check if all openingTimes are set
+    const formattedOpeningHours = openingTimes.map(time => {
+      if (time) {
+        const date = new Date(time);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        return `${hours}:${minutes}`;
+      }
+      return '';
+    }).join(",");
+  
+    setFormValue("opening_hours", formattedOpeningHours);
+    console.log("Opening hours: ", formattedOpeningHours);
+  }
+}, [openingTimes]);
+  
+useEffect(() => {
+  if (closingTimes.every(time => time)) { // Check if all openingTimes are set
+    const formattedClosingHours = closingTimes.map(time => {
+      if (time) {
+        const date = new Date(time);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        return `${hours}:${minutes}`;
+      }
+      return '';
+    }).join(",");
+  
+    setFormValue("closing_hours", formattedClosingHours);
+    console.log("closing_hours: ", formattedClosingHours);
+  }
+}, [closingTimes]);
 
   return (
     <form autoComplete="off" dir="rtl">
@@ -88,56 +96,51 @@ function FormOpeningHours(values, handleChange) {
         <Grid container spacing={22} mr={24}>
         {days.map((day, index) => (
             <Grid item xs={1} key={day}>
-    <LocalizationProvider dateAdapter={AdapterDayjs} >
-      <Box gap={2} display= 'flex' flexDirection='column'  gridTemplateColumns= '1fr 3fr 3fr' paddingTop={2} paddingRight={2} paddingLeft={2} sx={{direction: 'rtl'}} direction= 'row' alignItems= 'center'>
-        <Typography variant='h5' fontFamily="aleph" color= 'rgb(2, 0, 99)' fontSize={20} fontWeight= {540}>
-          {day}
-          </Typography>
-      <Box position="relative">
-      <Paper>
-        <TimeField
-          direction= 'rtl'
-          label="פתיחה"
-          value={null}
-          onChange={(event) => handleChangeOpeningTime(index, event)}
-          //value={value}
-          //onChange={(newValue) => setValue(newValue)}
-          format='HH:mm'
-          sx={{
-          "& label":{left: "unset",
-          right: "1.75rem",
-          transformOrigin: "right"},
-          "& legend": {
-            textAlign: "right",
-          },
-          width: '150px',
-          }}
-        />
-        </Paper>
-      {/* <button style={{position: 'absolute', bottom: 0, left: 0}} onClick={() => resetOpeningTime(index)}>X</button> */}
-      </Box>
-      <Paper>
-        <TimeField
-          label="סגירה"
-          value={null}
-          onChange={(event) => handleChangeClosingTime(index, event)}
-          //value={value2}
-          //onChange={(newValue) => setValue2(newValue)}
-          format='HH:mm'
-          sx={{
-          "& label":{left: "unset",
-          right: "1.75rem",
-          transformOrigin: "right"},
-          "& legend": {
-            textAlign: "right",
-          },
-          width: '150px',
-          }}
-        />
-        </Paper>
-      </Box>
-    </LocalizationProvider>
-    </Grid>
+              <LocalizationProvider dateAdapter={AdapterDayjs} >
+                <Box gap={2} display= 'flex' flexDirection='column'  gridTemplateColumns= '1fr 3fr 3fr' paddingTop={2} paddingRight={2} paddingLeft={2} sx={{direction: 'rtl'}} direction= 'row' alignItems= 'center'>
+                  <Typography variant='h5' fontFamily="aleph" color= 'rgb(2, 0, 99)' fontSize={20} fontWeight={540}>
+                    {day}
+                  </Typography>
+                  <Box position="relative">
+                    <Paper>
+                      <TimeField
+                        direction= 'rtl'
+                        label="פתיחה"
+                        value={openingTimes[index]}
+                        onChange={(newValue) => handleChangeOpeningTime(index, newValue)}
+                        format='HH:mm'
+                        sx={{
+                          "& label":{left: "unset",
+                          right: "1.75rem",
+                          transformOrigin: "right"},
+                          "& legend": {
+                            textAlign: "right",
+                          },
+                          width: '150px',
+                        }}
+                      />
+                    </Paper>
+                  </Box>
+                  <Paper>
+                    <TimeField
+                      label="סגירה"
+                      value={closingTimes[index]}
+                      onChange={(newValue) => handleChangeClosingTime(index, newValue)}
+                      format='HH:mm'
+                      sx={{
+                        "& label":{left: "unset",
+                        right: "1.75rem",
+                        transformOrigin: "right"},
+                        "& legend": {
+                          textAlign: "right",
+                        },
+                        width: '150px',
+                      }}
+                    />
+                  </Paper>
+                </Box>
+              </LocalizationProvider>
+            </Grid>
         ))}
         </Grid>
       </Box>
