67249aa6b26ff520ce0f58060e55443364347706
nicolesofias <nicolesofia@mail.tau.ac.il>
2023-06-27 15:45:30 +0300

    token passed to settings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -80,7 +80,7 @@ function App() {
                 } />
                 <Route path="/settings" element={
                   <>
-                    <ProfileSettings />
+                    <ProfileSettings token={token} setToken={setToken} />
                   </>
                 } />
               </>
