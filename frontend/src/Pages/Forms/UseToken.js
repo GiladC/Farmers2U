1f67b46226e14a346568f2ddd1a087ffd35e4360
TamirSadovsky <61319079+TamirSadovsky@users.noreply.github.com>
2023-08-14 23:52:06 +0300

    Update UseToken.js

--- frontend/src/Pages/Forms/UseToken.js
+++ frontend/src/Pages/Forms/UseToken.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import jwt_decode from 'jwt-decode';
 
 function UseToken() {
   const getToken = () => {
@@ -16,6 +17,36 @@ function UseToken() {
     localStorage.removeItem('token');
     setToken(null);
   };
+
+  const isTokenExpired = () => {
+    const token = localStorage.getItem('token');
+    if (!token) return false;
+
+    const decodedToken = jwt_decode(token);
+    const currentTime = Date.now() / 1000;
+
+    return decodedToken.exp < currentTime;
+  };
+
+  useEffect(() => {
+    if (isTokenExpired()) {
+      removeToken();
+      window.location.href = '/'; 
+    }
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isTokenExpired()) {
+        removeToken();
+        window.location.href = '/'; 
+      }
+    }, 60000); // Check every minute
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []); // Empty dependency array, so this effect only runs once
 
   return {
     token,
