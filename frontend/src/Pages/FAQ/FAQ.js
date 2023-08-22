444812c5630c039d7ab4d606028367c41c991cc4
raipheterson <96655166+raipheterson@users.noreply.github.com>
2023-08-22 20:53:54 +0300

    sectioned

--- frontend/src/Pages/FAQ/FAQ.js
+++ frontend/src/Pages/FAQ/FAQ.js
@@ -3,25 +3,22 @@ import styles from './FAQ.module.css'; // Import the CSS module
 
 
 function FAQ () {
-    const [faqs, setFaqs] = useState([
+    const [faqs1, setFaqs1] = useState([
         {
             question: 'אני בעל משק חקלאי ומעוניין לפרסם באתר. מה עליי לעשות?',
             answer: 'אם אינך רשום לאתר, ראשית עליך לפתוח פרופיל משתמש. לחיצה על כפתור "יצירת משתמש" תחל את הליך הרשמתך. אם הינך רשום לאתר, עליך להתחבר למשתמש דרכו נרשמת ולאחר מכן תוכל לפרסם מודעות תחת לשונית "לוח המודעות".',
             open: false
         },
         {
-            question: 'האם ישנה אפשרות לשלם לחקלאי ישירות מהאתר?',
-            answer: 'התשלום עבור הרכישה מתבצע ישירות מול החקלאי. לשם כך, יש ליצור קשר עם בית העסק ולברר מהו אופן התשלום. נכון לכרגע, אין אפשרות לשלם ישירות מהאתר.',
+            question: 'ניסיתי לפתוח פרופיל משתמש וראיתי כי ההרשמה מיועדת לחקלאים. כיצד ניתן לפתוח פרופיל צרכן?',
+            answer: 'לא ניתן, פתיחת פרופיל משתמש מוגבלת לחקלאים בלבד.',
             open: false
         },
+    ]);
+    const [faqs2, setFaqs2] = useState([
         {
             question: 'ראיתי באתר פרסום לגבי אירוע ואני מעוניין לברר עוד פרטים. מה עליי לעשות?',
             answer: 'ניתן ללחוץ על תמונת בית העסק או שם בית העסק המוצג במודעה שפורסמה. בעת הלחיצה תיפתח חלונית עם פרטי בית העסק, כעת ניתן לפנות ישירות לבית העסק באחד מאמצעי ההתקשרות שפורסמו על ידו ולברר את הפרטים הרצויים.',
-            open: false
-        },
-        {
-            question: 'ניסיתי לפתוח פרופיל משתמש וראיתי כי ההרשמה מיועדת לחקלאים. כיצד ניתן לפתוח פרופיל צרכן?',
-            answer: 'לא ניתן, פתיחת פרופיל משתמש מוגבלת לחקלאים בלבד.',
             open: false
         },
         {
@@ -35,33 +32,56 @@ function FAQ () {
             open: false
         },
         {
-            question: 'כיצד ניתן לברר אילו בתי עסק מבצעים משלוחים לאזורי?',
-            answer: 'יש להיכנס לדף "החקלאים שלנו", לבחור באפשרויות הצריכה "משלוחים", להזין את כתובתך וללחוץ על "הפעלת סינון". כעת יוצגו עבורך רק בתי העסק המבצעים משלוחים לאזורך.',
-            open: false
-        },
-        {
             question: 'אני ומשפחתי נוסעים לטייל ומעוניינים לדעת האם ישנם אירועי מכירה באזור הטיול. האם ניתן לברר זאת?',
             answer: 'כן. תחת לוח המודעות, יש להזין את טווח התאריכים הרצוי, כתובת אזור הטיול והמרחק ממיקום האירוע כפי שפורסם במודעה. לחיצה על "הפעלת סינון" תציג עבורך פרסומים הרלוונטיים לתאריכי הטיול ולאזור הטיול.',
             open: false
         }
     ]);
+    const [faqs3, setFaqs3] = useState([
+        {
+            question: 'האם ישנה אפשרות לשלם לחקלאי ישירות מהאתר?',
+            answer: 'התשלום עבור הרכישה מתבצע ישירות מול החקלאי. לשם כך, יש ליצור קשר עם בית העסק ולברר מהו אופן התשלום. נכון לכרגע, אין אפשרות לשלם ישירות מהאתר.',
+            open: false
+        },
+        {
+            question: 'כיצד ניתן לברר אילו בתי עסק מבצעים משלוחים לאזורי?',
+            answer: 'יש להיכנס לדף "החקלאים שלנו", לבחור באפשרויות הצריכה "משלוחים", להזין את כתובתך וללחוץ על "הפעלת סינון". כעת יוצגו עבורך רק בתי העסק המבצעים משלוחים לאזורך.',
+            open: false
+        },
+    ]);
 
-    const toggleFAQ = index => {
-        setFaqs(faqs.map((faq, i) => {
+    const toggleFAQ = (currentFaqs, setFaqsFunction, index) => {
+        setFaqsFunction(currentFaqs.map((faq, i) => {
             if (i === index) {
-                faq.open = !faq.open
+                faq.open = !faq.open;
             } else {
                 faq.open = false;
             }
             return faq;
-        }))
+        }));
     }
 
     return (
         <div dir="rtl" className={styles.faqs}>
-            {faqs.map((faq, i) => (
-                <FAQItem faq={faq} index={i} toggleFAQ={toggleFAQ} key={i} />
-            ))}
+            {/* <h1 className={styles.pageTitle}>שאלות נפוצות</h1> */}
+            <section>
+                <h2 className={styles.sectionTitle}>הרשמה לאתר</h2>
+                {faqs1.map((faq, i) => (
+                    <FAQItem faq={faq} index={i} toggleFAQ={() => toggleFAQ(faqs1, setFaqs1, i)} key={i} />
+                ))}
+            </section>
+            <section>
+                <h2 className={styles.sectionTitle}>לוח מודעות, אירועים ופרסומים</h2>
+                {faqs2.map((faq, i) => (
+                    <FAQItem faq={faq} index={i} toggleFAQ={() => toggleFAQ(faqs2, setFaqs2, i)} key={i} />
+                ))}
+            </section>
+            <section>
+                <h2 className={styles.sectionTitle}>רכישת מוצרים ומדיניות משלוחים</h2>
+                {faqs3.map((faq, i) => (
+                    <FAQItem faq={faq} index={i} toggleFAQ={() => toggleFAQ(faqs3, setFaqs3, i)} key={i} />
+                ))}
+            </section>
         </div>
     );
 }
